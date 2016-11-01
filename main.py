086f869fb4b51e9ab43be026aeba95e0bc378c93
ilyakmet <ilyakmet@gmail.com>
2016-11-01 21:00:14 +0300

    v0.02
    1. Use ThreadPool
    2. Phone number search

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 import search_api
 import pandas as pd
+from multiprocessing.dummy import Pool as ThreadPool
 
-query = 'строительные материалы киров'
+
+query = 'строительные материалы москва'
+pages_count = 1 #number of search pages
+pool_number = 5 #number of pools
+
+
+def main(url):
+	res = search_api.get_contacts(url)
+	if (res != set()) and (res != ()):
+		D[url] = str(res).strip("[]").replace("'", "").replace(' ', '')
 
 
 
 
 if __name__ == "__main__":
-    pd.Series({x:str(search_api.get_email_by_url(x)).strip('{}').replace("'", "") for x in search_api.get_yandex_urls_by_sup(query=query, sup=10)}).to_csv(query +'.csv')
-
-
-
+    D = {} #all data
+    pool = ThreadPool(pool_number)
+    pool.map(main, search_api.get_yandex_urls_by_sup(query=query, sup=pages_count))
+    pd.Series(D).to_csv(query.replace(' ', '_') +'.csv')
 
 
 
--- search_api.py
+++ search_api.py
@@ -1,14 +1,19 @@
 from grab import Grab
 import re, os, logging
 
+
+mail_regex = r'\b[\w.-]+?@\w+?\.\w+?\b'
+phone_regex = r'\b[8-9]{0,2}[\-\(\)\d\s]{9,18}\b'
+
+
 DIR = os.path.abspath(os.curdir) + '/'
 logging.basicConfig(level=logging.DEBUG)
+
 
 def get_yandex_urls_by_page_num(query, page=0):
     try:
         g = Grab()
-        g.proxylist.load_file(DIR + 'proxy.txt')
-        #g.setup(proxy_auto_change=False)
+        #g.proxylist.load_file(DIR + 'proxy.txt') #for proxy
         g.go('http://yandex.ru/yandsearch?p=%s&text=%s' % (page, query))
         #print(g.response.body)
         return [x.attr('href') for x in g.doc.select('//a[@class="link organic__url link link_cropped_no"]')]
@@ -30,9 +35,25 @@ def get_email_by_url(url):
     try:
         g = Grab()
         g.go(url)
-        return set(re.findall(r'\b[\w.-]+?@\w+?\.\w+?\b', str(g.response.body)))
+        return set(re.findall(mail_regex, str(g.response.body)))
     except:
         print('get_email_by_url error')
+        return ()
+
+def get_phone_by_url(url):
+    try:
+        g = Grab()
+        g.go(url)
+        return set(re.findall(phone_regex, str(g.response.body)))
+    except:
+        print('get_phone_by_url error')
+        return ()
+
+def get_contacts(url):
+    try:
+        return list(get_email_by_url(url)) +  list(get_phone_by_url(url))
+    except:
+        print('get_contacts error')
         return ()
 
 
